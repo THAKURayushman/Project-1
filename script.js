7f45ea3ec065fdf8ad9e2ba1296617f96daa6503
Ayushman Thakur <thakurayushman333@gmail.com>
2024-05-28 21:41:53 +0530

    page3 completed

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector(".main"),
   smooth: true,
 });
+
+let tabs = document.querySelector("#tabs");
+let fixedImage = document.querySelector(".fixedImage");
+tabs.addEventListener("mouseenter", () => {
+  fixedImage.style.display = "block";
+});
+
+tabs.addEventListener("mouseleave", () => {
+  fixedImage.style.display = "none";
+});
+
+let bar = document.querySelectorAll(".bar");
+bar.forEach((t) => {
+  t.addEventListener("mouseenter", () => {
+    let img = t.getAttribute("dataImage");
+    fixedImage.style.backgroundImage = `url(${img})`;
+  });
+});
